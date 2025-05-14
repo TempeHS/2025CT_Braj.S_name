7ede987410fb2eed4c30544e1ed3768b5713a38d
Braj.S <braj.sapkota@education.nsw.gov.au>
2025-05-15 09:38:17 +1000

    Edited Enemy.cs

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 2f
+
+    private Rigidbody2D rb; 
+
+    [SerializeField] private Transform checkpoint;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Vector2.Distance(checkpoint.transform.position,transform.position) <= 0.1f)
+        {
+            Debug.Log("Checkpoint Reached");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = (checkpoint.position - transform.position).normalized;
+        rb.velocity = direction * movespeed;
+    }
+}
