13d60f3bb8d4bda98974757a63dfb7a8f0996dbe
Braj.S <braj.sapkota@education.nsw.gov.au>
2025-06-26 09:38:32 +1000

    Updated EnemyManager, running into errors

--- Assets/_Scripts/EnemyManager.cs
+++ Assets/_Scripts/EnemyManager.cs
@@ -5,11 +5,73 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager main;
+
+    public Transform spawnpoint;
     public Transform[] checkpoints;
 
+    [SerializeField] private GameObject zombie;
+    [SerializeField] private GameObject fastZombie;
+    [SerializeField] private GameObject tankZombie;
+
+    [SerializeField] private int enemyCount = 6;
+
+    [SerializeField] private float zombieRate = 0.5f;
+    [SerializeField] private float fastZombieRate = 0.4f;
+    [SerializeField] private float tankZombieRate = 0.1f;
+
+    private List<GameObject> waveset = new List<GameObject>();
+    private int enemyLeft;
+
+    private int zombieCount;
+    private int fastZombieCount;
+    private int tankZombieCount;
     void Awake()
     {
         main = this;
     }
 
-}
+    void Start()
+    {
+        Setwave();
+    }
+
+    void Update();
+    {
+        
+    }
+    
+    private void SetWave()
+    {
+        zombieCount = Mathf.RoundToInt(enemyCount * zombieRate);
+        fastZombieCount = Mathf.RoundToInt(enemyCount * fastZombieRate);
+        tankZombieCount = Mathf.RoundToInt(enemyCount * tankZombieRate);
+
+        waveset = new List<gameObject>();
+
+        for (int i = 0; i < zombieCount; i++)
+        {
+            waveset.Add(zombie);
+        }
+
+        for (int i = 0; i < fastZombieCount; i++)
+        {
+            waveset.Add(fastZombie);
+        }
+
+        for (int i = 0; i < tankZombieCount; i++)
+        {
+            waveset.Add(tankZombie);
+        }
+
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        for (int i = 0; i < waveset.Count; i++)
+        {
+            Instantiate(waveset[i], spawnpoint.position, Quaternion.identity);
+            yield return new waitForSeconds(0.5f);
+        }
+    }
+}   
